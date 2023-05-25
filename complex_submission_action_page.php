7ca42985d46d0ba6d7ea40b54b60d395a851027f
Constantinos Tefkros Loizou <cloizo07@ada.in.cs.ucy.ac.cy>
2023-05-25 13:06:24 +0300

    Special input slides - Now escaping special chars and preventing from SQL injection

--- complex_submission_action_page.php
+++ complex_submission_action_page.php
@@ -49,10 +49,10 @@ if ($num_rows > 0) {
     }
 }
 
-// Now create record and insert it to DB
-$sql = "INSERT INTO eplatform_iN_SPECIAL_INPUT (answer_id, user_id, slide_id, `0_0`, `0_1`, `0_2`, `0_3`, `0_4`, `0_5`, `0_6`, `0_7`, `0_8`, `1_0`, `1_1`, `1_2`, `1_3`, `1_4`, `1_5`, `1_6`, `1_7`, `1_8`) 
-    VALUES (NULL, '$user_id', '$slide_id', '$c00', '$c01', '$c02', '$c03', '$c04', '$c05', '$c06', '$c07', '$c08', '$c10', '$c11', '$c12', '$c13', '$c14', '$c15', '$c16', '$c17', '$c18')";
-$result = mysqli_query($con, $sql);
+// Now create record and insert it to DB - Also escape special chars and prevent injections
+$stmt   = $con->prepare("INSERT INTO eplatform_iN_SPECIAL_INPUT (answer_id, user_id, slide_id, `0_0`, `0_1`, `0_2`, `0_3`, `0_4`, `0_5`, `0_6`, `0_7`, `0_8`, `1_0`, `1_1`, `1_2`, `1_3`, `1_4`, `1_5`, `1_6`, `1_7`, `1_8`) VALUES (NULL, (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?), (?)) ");
+$result = $stmt->bind_param("isssssssssssssssssss", $user_id, $slide_id, $c00, $c01, $c02, $c03, $c04, $c05, $c06, $c07, $c08, $c10, $c11, $c12, $c13, $c14, $c15, $c16, $c17, $c18);
+$result = $stmt->execute();
 
 if ( $result ){
     // Success - Redirect to next slide
@@ -78,6 +78,8 @@ if ( $result ){
 // Insertion failed
 else {
     echo "Could not insert answers into DB";
+    echo "<br>";
+//    echo $stmt->error;
     die();
 }
 
